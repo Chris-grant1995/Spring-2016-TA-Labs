789dd55beaa188022616cec372a762ad917c295c
Chris Grant <cgrant1995@Me.com>
2016-01-11 11:23:49 -0500

    Lab1

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+bill = float(input("Please provide the total amount for the bill: "))
+avg = bill * .15
+good = bill * .20
+great = bill * .25
+
+print('Your bill was {0:.2f}:\nAvg Tip = ${1:.2f}\n\
+Good Tip = ${2:.2f}\nGreat Tip = ${3:.2f}'.format(bill,avg,good,great))
+
+
+
